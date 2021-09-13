6839722d01bcadca65d227670d6bddb4ec1b05ae
giovxna <gi.siqueira0202@gmail.com>
2021-09-13 15:08:25 -0300

    upando o projeto

---
+++ README.md
@@ -0,0 +1,15 @@
+![image](assets/fintech.png)
+
+## 🌈 desafio
+
+construa uma aplicação que fará uma simulação de investimento utilizando juros composto. a aplicação terá um formulário com os campos: nome, mensalidade, tempo de contribuição e um botão simular. exiba um texto com as informações dos campos de nome, mensalidade, tempo e o resultado da requisição. a requisição em si não precisa ser programada nem mesmo é necessário qualquer calculo de juros, mas pode ser um bônus. 
+
+## ✨ tecnologias
+
+esse projeto foi desenvolvido com as seguintes tecnologias:
+
+- HTML
+- CSS
+- Javascript
+
+
---
+++ script.js
@@ -0,0 +1,27 @@
+// puxando os dados do formulário
+const simular = document.getElementById('simular');
+
+function simulador() {
+    const input_nome = document.getElementById('input_nome').value;
+    const input_mensalidade = document.getElementById('input_mensalidade').value;
+    const input_tempo = document.getElementById('input_tempo').value;
+    const resultado = document.getElementById('resultado');
+
+    // repetição das variavéis para resetar o formulário
+    document.getElementById('input_nome').value = ""
+	document.getElementById('input_mensalidade').value = "";
+	document.getElementById('input_tempo').value = "";
+
+    // validação do formulário
+    if (input_nome !== '' && input_mensalidade !== '' && input_tempo !== '') {
+        // tofixed serve para arredondar as casas decimais
+        const calculo = (input_tempo * 12 * input_mensalidade).toFixed(2);
+        resultado.textContent = `Olá, ${input_nome}! Juntando R$ ${input_mensalidade} todo mês, você terá ${calculo} em ${input_tempo} ano(s).`;
+    } else {
+        resultado.textContent = "Preencha todos os campos.";
+    }
+}
+
+// quando o usuário cliclar no botão de "simular", o evento de clique de
+// vai acontecer e pegará os dados informados
+simular.addEventListener('click', simulador);
